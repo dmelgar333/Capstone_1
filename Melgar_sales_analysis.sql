ba2eb748aaac082923180b3fc005a13551c3f412
dmelgar333 <dmelgar@my.yearupunited.org>
2026-04-28 21:53:28 -0400

    update maine sales analysis queries

--- Melgar_sales_analysis.sql
+++ Melgar_sales_analysis.sql
@@ -17,3 +17,16 @@ JOIN store_locations sl ON ss.Store_ID = sl.StoreId
 WHERE sl.state = 'Maine';
 
 -- Results: In the Maine territory, the data timeframe is from January 1st, 2022 to Decemeber 31st, 2025. Total revenue during this time was $1,877,249.75
+
+-- Question 2: What is the month by month revenue breakdown for the sales territory?
+
+SELECT
+ MONTH(Transaction_Date) AS Month_Num,
+ MONTHNAME(Transaction_Date) AS Month_Name,
+ YEAR(Transaction_Date) AS Year,
+ ROUND(SUM(Sale_Amount), 2) AS Monthly_Revenue
+FROM store_sales ss
+JOIN store_locations sl ON ss.Store_ID = sl.StoreId
+WHERE sl.State = 'Maine'
+GROUP BY YEAR(Transaction_Date), MONTH(Transaction_Date), MONTHNAME(Transaction_Date)
+ORDER BY YEAR(Transaction_Date), MONTH(Transaction_Date);
